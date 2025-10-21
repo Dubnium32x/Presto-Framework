42ed1d59a473270c9ee24daf6d8a2f8a2d4d0145
Dubnium32x <63617522+Dubnium32x@users.noreply.github.com>
2025-10-21 10:26:47 -0700

    added new tilesets and title card gfx

---
+++ res/data/levels/LEVEL_0/atlantis3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="atlantis3" tilewidth="16" tileheight="16" tilecount="80" columns="10">
+ <image source="../../../sprite/spritesheet/tileset/atlantis3.png" width="160" height="128"/>
+</tileset>
