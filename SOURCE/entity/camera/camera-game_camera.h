faf589e59ca583cd879963abf2fb41294628275d
Dubnium32x <63617522+Dubnium32x@users.noreply.github.com>
2025-12-21 13:00:56 -0800

    added tools

---
+++ SOURCE/entity/camera/camera-game_camera.h
@@ -0,0 +1,44 @@
+// Game camera header
+#ifndef CAMERA_GAME_CAMERA_H
+#define CAMERA_GAME_CAMERA_H
+
+#include "raylib.h"
+#include <stdio.h>
+
+typedef struct {
+    Vector2 position;
+    Vector2 targetPos;
+    float zoom;
+    float rotation;
+
+    // SPG camera properties
+    Vector2 borders;
+    float verticalFocalPoint;
+    float horizontalFocalPoint;
+
+    // Camera movement speedcaps
+    float horizontalSpeedCap;
+    float verticalSpeedCap;
+    float defaultVerticalSpeedCap;
+
+    // Look up/down properties
+    bool isLookingUp;
+    bool isLookingDown;
+    int lockTimer;
+    float verticalShift;
+
+    // Extended camera (CD only)
+    float extendedHorizontalShift;
+    float extendedVerticalShift;
+    bool extendedCameraActive;
+
+    // Screen shake
+    float screenShakeIntensity;
+    float screenShakeDuration;
+    float screenShakeTimer;
+} GameCamera;
+
+GameCamera GameCamera_Init(int xPos, int yPos, int tX, int tY, float zoom, float rot);
+void MoveCamTo(GameCamera* cam, Vector2 newPos);
+
+#endif // CAMERA_GAME_CAMERA_H
